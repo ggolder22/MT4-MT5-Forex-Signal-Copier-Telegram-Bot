9b1175d3240e4116fb098d8e48501a3d3bc70700
ggolder22 <germanxvzy985@hotmail.com>
2023-05-15 11:18:43 -0300

    p

--- run.py
+++ run.py
@@ -282,10 +282,10 @@ async def ConnectMetaTrader(update: Update, trade: dict, enterTrade: bool):
             try:
                 # executes buy market execution order
                 if(trade['OrderType'] == 'Buy'):
-                    #for takeProfit in trade['TP']:
+                    for takeProfit in trade['TP']:
                         #result = await connection.create_market_buy_order(trade['Symbol'], trade['PositionSize'] / len(trade['TP']), trade['StopLoss'], takeProfit)
                         #result = await connection.create_market_buy_order(trade['Symbol'], trade['PositionSize'] / len(trade['TP']), trade['StopLoss'], takeProfit)
-                        result = await connection.create_market_buy_order(trade['Symbol'], trade['SIZE'], trade['StopLoss'], trade["TP"])
+                        result = await connection.create_market_buy_order(trade['Symbol'], trade['SIZE'][0], trade['StopLoss'], takeProfit)
                         
                         
                 # # executes buy limit order
