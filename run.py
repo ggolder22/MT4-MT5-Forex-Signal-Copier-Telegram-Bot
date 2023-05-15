95aee59976d152d11b3847420ee89ff7b51aac80
ggolder22 <germanxvzy985@hotmail.com>
2023-05-15 13:05:07 -0300

    49

--- run.py
+++ run.py
@@ -283,7 +283,7 @@ async def ConnectMetaTrader(update: Update, trade: dict, enterTrade: bool):
                 # executes buy market execution order
                 if(trade['OrderType'] == 'Buy'):
                     
-                    for index,takeProfit in trade['TP']:
+                    for takeProfit in trade['TP']:
                         result = await connection.create_market_buy_order(trade['Symbol'], trade['PositionSize'] / len(trade['TP']), trade['StopLoss'], takeProfit)
                         
                         #result = await connection.create_market_buy_order(trade['Symbol'],  (trade['SIZE'][index],), trade['StopLoss'], takeProfit)
